8b6bbadfdfe39fca057c869225edcaebd69d9579
daddyrusher <daddyrusher@gmail.com>
2019-07-23 18:43:53 +0500

    initial commit

---
+++ script.py
@@ -0,0 +1,19 @@
+w = 10
+h = 15
+
+
+def area(w, h):
+    return w * h
+
+
+area(w, h)
+
+'''
+    Multi
+    line
+    comment
+'''
+
+# lambda expression
+sum = lambda arg1, arg2: arg1 + arg2
+print('Value of total', sum(60, 40))
